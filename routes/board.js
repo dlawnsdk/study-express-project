35c31cfa327f28651d73fcd5b3d7d6aa0cdbbd3f
Juna Im <dlawnsdk04@naver.com>
2022-12-08 20:50:54 +0900

    자바스크립트 폴더 분리중

--- routes/board.js
+++ routes/board.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/board', function(req, res, next){
-    console.log("test " + req)
+router.get('/', function(req, res, next) {
     res.render('board', { title: '자유게시판' })
 });
 
