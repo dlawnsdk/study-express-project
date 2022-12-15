7994d7874d2c1dd5eeeb57f208bd44ede4ba2796
Juna Im <dlawnsdk04@naver.com>
2022-12-15 21:53:49 +0900

    1. 쿼리 조회 하여 리스트 출력까지 성공 2. 데이터 저장 성공 3. 성공 후 리턴 페이지 구현중

--- models/boardDao/boardList.js
+++ models/boardDao/boardList.js
@@ -2,12 +2,11 @@ var express = require('express');
 
 /**
  * db connection
- */
 const connection = require('../../lib/dataBase')
 
 exports.boardList = connection.query(
     "SELECT ID, NAME FROM USER"
 ).then((result) => {
     console.log(result[0])
-})
+})*/
 
--- routes/board.js
+++ routes/board.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var router = express.Router();
 const db = require('../models/boardDao/boardList')
+const connection = require('../lib/dataBase')
 
 // app.get으로 요청하는 경우 URL 적시 <-> app.use인 경우 / 만 적시
 router.get('/board', function(req, res, next) {
-    db.
-    res.render('board/list', { title: '자유게시판' })
+
+    connection.query(
+        "SELECT IDX, TITLE, CONTENTS FROM BOARD"
+    ).then((result) => {
+        res.render('board/list', { list: result[0] })
+    })
 });
 
 router.get('/board/edit', function(req, res, next) {
@@ -13,10 +18,18 @@ router.get('/board/edit', function(req, res, next) {
 });
 
 router.post('/board/save', function(req, res, next) {
-    console.log("test")
-    console.log(req.body.title)
-    console.log(req.body.contents)
-    res.render('board/list', { title: '자유게시판' })
+
+    var board = {
+        idx: 3,
+        title: req.body.title,
+        contents: req.body.contents
+    }
+    connection.query(
+        //"INSERT INTO BOARD(IDX, TITLE, CONTENTS) VALUES ( ?, ?, ?)", [3, req.body.title, req.body.contents]
+        "INSERT INTO BOARD SET ?", board
+    ).then((result) => {
+        res.render('/board')
+    })
 });
 
 module.exports = router;
